49492c988a550ea4fbd70b0aed13901d819cd8bd
Anushek Prasal <anushekprasal@gmail.com>
2020-04-15 15:30:54 +0530

    base: Add config to force enable hardware keys
    On some devices the assist key or some other key doesn't work until the enable hardware keys toggle is turned on. This is an issue if the device doesn't have any other hardware key. This should allow maintainers to enable it for their device the easy way.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -422,6 +422,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean mANBIEnabled;
 
     private boolean mHwKeysEnabled = true;
+    private boolean mForceEnableKeys;
 
     boolean mVolumeRockerWake;
     private boolean mVolumeMusicControlActive;
@@ -2186,6 +2187,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mDeviceHardwareKeys = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_deviceHardwareKeys);
 
+        mForceEnableKeys = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_forceEnableHardwareKeys);
+
         mHandleVolumeKeysInWM = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_handleVolumeKeysInWindowManager);
 
@@ -2351,11 +2355,12 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     public boolean isHwKeysDisabled() {
-        return !mHwKeysEnabled;
+        return !mHwKeysEnabled && !mForceEnableKeys;
     }
 
     private boolean filterDisabledKey(int keyCode) {
-        return !mHwKeysEnabled && (keyCode == KeyEvent.KEYCODE_HOME
+        return !mHwKeysEnabled && !mForceEnableKeys
+                && (keyCode == KeyEvent.KEYCODE_HOME
                 || keyCode == KeyEvent.KEYCODE_MENU
                 || keyCode == KeyEvent.KEYCODE_APP_SWITCH
                 || keyCode == KeyEvent.KEYCODE_ASSIST
